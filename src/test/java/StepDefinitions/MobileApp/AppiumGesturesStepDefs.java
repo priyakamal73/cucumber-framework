c386662bc4f81416d2c62b918f8ad26de0e544d6
Sanmukapriya <priyakamal5391@gmail.com>
2025-02-12 17:38:42 +0530

    Modified the name of gesture step def file

--- src/test/java/StepDefinitions/MobileApp/AppiumGesturesStepDefs.java
+++ src/test/java/StepDefinitions/MobileApp/AppiumGesturesStepDefs.java
@@ -6,7 +6,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class AppiumGestures {
+public class AppiumGesturesStepDefs {
 
     GesturesPage gesturesPage = new GesturesPage(Hooks.getAndroidDriver());
 
