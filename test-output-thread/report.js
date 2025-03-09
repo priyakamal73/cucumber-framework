ae061a548ae60dc32f7c9cfb30a9b675095895b5
Sanmukapriya <priyakamal5391@gmail.com>
2025-03-10 01:39:27 +0530

    Deleted Jenkinsfile

--- test-output-thread/report.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"04625882-9514-4bfd-b49e-b2823dda493f","feature":"OTP Login","scenario":"Successful login using OTP","start":1739012890153,"group":1,"content":"","tags":"@webapp,@otp,","end":1739012964533,"className":"passed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[#1,main,5,main]"}]);
-});
